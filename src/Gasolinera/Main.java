7441e41ef1124d142638660b5534ea0914ab6f63
Joowyy <joelsanchezfernandez05@gmail.com>
2025-03-12 01:10:16 +0100

    Menu Hecho

--- src/Gasolinera/Main.java
+++ src/Gasolinera/Main.java
@@ -10,33 +10,57 @@ public class Main {
 //		AÑADIR METODO DE ELIMINAR TODOS
 //		LOS OBJETOS DE LA ARRAY EN TRUE
 		
-		
-//		Movil movil = new Movil();
-//		
-//		movil.cargar();
-		
+//		Gasolinera principal
 		Gasolinera g = Gasolinera.crearGasolinera();
-		
-		g.mostrarGasolinera();
-		
+
 //		Almacenamos la opcion en 'opcUsuario'
-		char opcUsuario;
-		opcUsuario = mostrarMenu();
+		char opcUsuario = mostrarMenu(g);
 		
 	}
 	
 //	MENU DEL PROGRAMA
-	public static char mostrarMenu () {
+	public static char mostrarMenu (Gasolinera g) {
 		
 		Scanner sc = new Scanner (System.in);
 		char opc;
 		
-		System.out.println("BIENVENIDO A REPSOL\n1. Añadir coche electrico\n2. Añadir coche gasolina\n3. Añadir movil\n4. Mostrar gasolinera\n");
-		switch (opc = sc.nextLine().charAt(0)) {
-		
-		
-		
-		}
+		do {
+
+			System.out.println("BIENVENIDO A REPSOL\n1. Añadir coche electrico\n2. Añadir coche gasolina\n3. Añadir movil\n4. Mostrar gasolinera\n5. Quitar dispositivos cargados\n6. Salir del programa");
+			switch (opc = sc.nextLine().charAt(0)) {
+			case '1':
+
+				break;
+
+			case '2':
+
+				break;
+
+			case '3':
+
+				break;
+
+			case '4':
+
+				g.mostrarGasolinera();
+				break;
+
+			case '5':
+
+				break;
+
+			case '6':
+
+				System.out.println("Saliendo del programa...");
+				break;
+
+			default:
+				
+				System.out.println("Introduce un carácter válido.\n");
+
+			}
+
+		} while (opc != '6');
 		
 		return opc;
 	}
